5b68375a82f153439ed9128487976a23fb70d00f
pcaserta <patrick.caserta@live.longwood.edu>
2019-08-12 14:57:07 -0400

    updated axios for security reasons

---
+++ index.js
@@ -0,0 +1,17 @@
+var input = 6;
+var array = [1,5,2,1,10];
+
+var aboveBelow =function(input,array){
+    
+for (var i = 1 < array.length; i++){
+    var above = 0;
+    var below = 0;
+    var currentNumber = array[i]
+    if(currentNumber < input){
+        above++
+        
+    }else {
+      return  below++
+    }
+    }
+}
